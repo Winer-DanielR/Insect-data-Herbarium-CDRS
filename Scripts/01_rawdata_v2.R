56ad0d710da0a7154cd9cf4aacb31aebbdaa585a
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2020-11-20 14:05:15 -0500

    Script borrado decidí usar el script de Sarita.

--- Scripts/01_rawdata_v2.R
+++
@@ -1,6 +0,0 @@
-################ Datos de insectos de CDRS #######################
-
-### Data exploration of insect dataset and environmental variables
-
-### Clean the data, check for NAs and get the data ready for analysis
-
