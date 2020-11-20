9106ffc73406e8138b77724b154534bf105d556a
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2020-11-20 13:59:51 -0500

    Script de librerías usadas en el análisis

--- Scripts/00_packages_v1.R
+++ Scripts/00_packages_v1.R
@@ -8,6 +8,9 @@ library(tidyverse) #For data cleaning and graphs
 library(lme4) #For modeling
 library(lmerTest)
 library(readr) #For reading cvs files
+library("lubridate")
+
+
 
 # library(RColorBrewer) #For PCA analysis
 # #library(ellipse)
