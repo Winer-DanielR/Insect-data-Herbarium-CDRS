9038f1b4bc0fb5ef6f4aa8bff63d4c13524a295f
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2020-12-14 17:15:35 -0500

    New version of libraries scripted. Used with the updated dataset.

---
+++ Scripts/00_packages_v2.R
@@ -0,0 +1,19 @@
+################ Insect Data CDRS #######################
+
+##### List of packages used for analysis
+### Package scripts
+
+
+library(tidyverse) #For data cleaning and graphs
+library(lme4) #For modeling
+library(car) #For model analysis
+library(readr) #For reading cvs files
+library("lubridate")
+
+
+
+# library(RColorBrewer) #For PCA analysis
+# #library(ellipse)
+# #library(rgl)
+# library(factoextra)
+# library(FactoMineR)
