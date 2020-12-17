95660975a7435089c121ad7e43a7f04982893764
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2020-12-17 15:48:06 -0500

    Included plotrix package for standard error estimates.

--- Scripts/00_packages_v2.R
+++ Scripts/00_packages_v2.R
@@ -9,7 +9,7 @@ library(lme4) #For modeling
 library(car) #For model analysis
 library(readr) #For reading cvs files
 library("lubridate")
-
+library(plotrix) #Standard error
 
 
 # library(RColorBrewer) #For PCA analysis
