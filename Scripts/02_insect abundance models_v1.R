bc1902357b9a83f014ed88df11a388838ef4ac3e
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2021-03-12 00:06:11 -0500

    Removi lineas para transformar datos. Es mejor transformar los datos en el modelo directamente.

--- Scripts/02_insect abundance models_v1.R
+++ Scripts/02_insect abundance models_v1.R
@@ -11,18 +11,24 @@ select(pest_data, fecha_puesta:fecha_recogida, year, everything())
 
 
 # Changed variables to factors and scaled temperature and humidity
-pest_data <- pest_data %>% mutate_at(vars(fecha_puesta, monitoreo, 
-                                          year, tipo_trampa, 
+pest_data <- pest_data %>% mutate_at(vars(monitoreo, 
+                                          tipo_trampa, marca_trampa,
                                           ubicacion), list(factor))
-pest_data$humedad_scaled <- scale(pest_data$hum_mean)
-pest_data$temperatura_scaled <-scale(pest_data$temp_mean)
-pest_data$log_abundancia_trampa <- log(pest_data$abundancia_trampa + 1)
-hist(pest_data$abundancia_trampa)
-hist(pest_data$log_abundancia_trampa)
+
+diagnostic(sqrt(pest_data$abundancia_trampa))
+
+
+
+
+# pest_data$humedad_scaled <- scale(pest_data$hum_mean)
+# pest_data$temperatura_scaled <-scale(pest_data$temp_mean)
+# pest_data$log_abundancia_trampa <- log(pest_data$abundancia_trampa + 1)
+# hist(pest_data$abundancia_trampa)
+# hist(pest_data$log_abundancia_trampa)
 
 #Exploring the data
 ggplot(pest_data) +
-  aes(x = year, y = log_abundancia_trampa, fill = familia) +
+  aes(x = year, y = abundancia_trampa, fill = familia) +
   geom_boxplot() +
   scale_fill_hue() +
   theme_minimal() +
@@ -34,11 +40,23 @@ str(pest_data)
 # FITTING A GLMM 
 
 ####### Modelo general considerando la humedad, la temperatura y la interaccion de humedad y temperatura ####
-full_model <- glmer(abundancia_trampa ~ humedad_scaled + temperatura_scaled + humedad_scaled*temperatura_scaled + 
-                      tipo_trampa + (1|monitoreo) + (1|num_trampa_monitoreo) + (1|especie) + (1|ubicacion), family=poisson, 
+full_model <- glmer(abundancia_trampa ~ log(hum_mean) + log(temp_mean) + log(temp_mean*hum_mean) + 
+                      (1|tipo_trampa) + (1|monitoreo) + (1|ubicacion), family=poisson, 
                     data=pest_data, control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+model <- glmmTMB::glmmTMB(abundancia_trampa ~ temp_mean + hum_mean 
+                          + temp_mean*hum_mean
+                          + monitoreo
+                          + (1|trampa_ID_unico)
+                          + (1|ubicacion), data = pest_data,
+                          ziformula = ~.,
+                          family = poisson)
 
+summary(model)
+Anova(full_model)
+hist(resid(full_model))
 
+plotResiduals(model)
+plotQQunif(model)
 #Diagnostic models
 plot(fitted(full_model),resid(full_model)) 
 abline(h=0,lty=2,col="red")
